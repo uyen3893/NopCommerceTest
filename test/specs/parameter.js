8128a79cb892fa1d0a72bd408209855ca9c9e9fd
uyen3893 <uyen3893@gmail.com>
2021-02-22 16:20:13 +0700

    Test Register page

---
+++ test/specs/parameter.js
@@ -0,0 +1,19 @@
+const firstname = 'uyen'
+const lastname = 'nguyen'
+const email = 'adain.48@dikybuyerj.com'
+const invalid_email ='test@email.com'
+const invalid_value = 'acb'
+const password = 'Test@123'
+const blank_value = ''
+var character = ''
+
+module.exports = {
+    firstname,
+    lastname,
+    email,
+    invalid_email,
+    invalid_value,
+    password,
+    blank_value,
+    character
+}
---
+++ test/specs/test_register_page.js
@@ -0,0 +1,51 @@
+const RegisterPage = require('../nop-page-objects/register.page')
+const parameter = require('./parameter')
+
+describe('Register page', () => {
+    before(() => {
+        RegisterPage.open()
+        RegisterPage.setFrame()
+    })
+
+    describe('Valid info when register', () => {
+        it('1. should register new account when inputting valid information', () => {
+            RegisterPage.register(parameter.firstname, parameter.lastname, parameter.email, parameter.password)
+            expect(RegisterPage.successful_register_message).toHaveText('Your registration completed')
+            RegisterPage.logout_tab.click()
+        })
+    })
+    
+    describe('Invalid info when register', () => {
+        it('2. should show error message when firstname, lastname, email and password are blank', () => {
+            RegisterPage.register(parameter.blank_value, parameter.blank_value, parameter.blank_value, parameter.blank_value)
+            expect(RegisterPage.firstname_message).toHaveText('First name is required.')
+            expect(RegisterPage.lastname_message).toHaveText('Last name is required.')
+            expect(RegisterPage.email_message).toHaveText('Email is required.')
+            expect(RegisterPage.password_message).toHaveText('Password is required.')
+            expect(RegisterPage.confirm_password_message).toHaveText('Password is required.')
+    
+        })
+    
+        it('3. should show error message when inputting invalid email', () => {
+            RegisterPage.register(parameter.blank_value, parameter.blank_value, parameter.invalid_value, parameter.blank_value)
+            expect(RegisterPage.email_message).toHaveText('Wrong email')
+        })
+    
+        it('4. should show error message when inputting password equal or less than 5 characters', () => {
+            for(var i = 1; i <= 5; i++) {
+                parameter.character += 'a'
+                RegisterPage.register(parameter.blank_value, parameter.blank_value, parameter.invalid_value, parameter.character)
+                expect(RegisterPage.wrong_password_message).toHaveText('must have at least 6 characters')
+            }
+            
+        })
+    
+        it('5. should show error message when inputting a registered email', () => {
+            RegisterPage.register(parameter.firstname, parameter.lastname, parameter.email, parameter.password)
+            expect(RegisterPage.email_exists_message).toHaveText('The specified email already exists')
+        })
+    })
+
+    
+})
+
